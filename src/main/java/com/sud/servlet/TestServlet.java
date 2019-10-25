c68d45b9ac6d4406f69591b491580a76788ebbb3
sudarshan.nagre <sudarshan.nagre@YP1010308DT.Yash.com>
2019-10-25 16:43:54 +0530

    test

---
+++ src/main/java/com/sud/servlet/TestServlet.java
@@ -0,0 +1,38 @@
+package com.sud.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String name = request.getParameter("name");
+		
+		PrintWriter pw = response.getWriter();
+		pw.print("<hi> Hello "+name);
+		
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+	<form action="test">
+		<h2>Hello Guest ...</h2>
+		<br> Guest Name : <input type="text"
+			placeholder="Enter Name Here" id="name" name="name" />
+			
+			<input type="submit" value="ClickMe"/>
+	</form>
+</body>
+</html>
